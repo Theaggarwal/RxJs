7e3472e99bb7b9865ae2b5350c05a6e2b069b858
SUSHIL AGGARWAL <theaggarwal1@gmail.com>
2018-01-13 07:58:16 +0530

    included RxJs module in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as Rx from 'rxjs/rx';
+// import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    // this.javascriptCode();
+    this.clickCounter();
+  }
+
+  javascriptCode() {
+    let count = 0;
+    const button = document.querySelector('button');
+    button.addEventListener('click', () => console.log(`Clicked ${++count} times`));
+  }
+
+  clickCounter() {
+    const button = document.querySelector('button');
+    Rx.Observable.fromEvent(button, 'click')
+      .scan((count: number) => count + 1, 0)
+      .subscribe(count => console.log(`Clicked ${count} times`));
+
+    // add the mouse x position for every click after 1000ms
+    Rx.Observable.fromEvent(button, 'click')
+      .throttleTime(1000)
+      .map((event: any) => event.clientX)
+      .scan((count, clientX) => count + clientX, 0)
+      .subscribe(count => console.log('position: ' + count + ' '));
+
+  }
 }
